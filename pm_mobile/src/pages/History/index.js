25b8464783a4310c7bf662b5393e11cb9a97285b
Leandro Santo <leandro.santo@outlook.com>
2020-06-13 18:50:34 -0300

    troca do Add-on do chart e implementação do mesmo.

--- pm_mobile/src/pages/History/index.js
+++ pm_mobile/src/pages/History/index.js
@@ -1,88 +1,83 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { LineChart, YAxis, XAxis, Grid } from 'react-native-svg-charts'
-import styles from '../Sign-in/styles';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import PureChart from 'react-native-pure-chart';
 
+import styles from '../History/styles';
 
 export default function History(){
-    const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
-    const data2 = [10, 50, 90, 5, -6, -70, 20, 10, 10, 20, -5, 10, 50, 20, -40]
-    const contentInset = { top: 20, bottom: 20 }
-
+    
+        // const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
+        var data = [
+                {seriesName: 'systolic', data: [12, 14, 18, 12, 20], color: 'red'},
+                {seriesName: 'diastolic', data: [8, 12, 12, 8, 18], color: 'blue'}
+        ]
+    /*    
+        const data2 = [10, 50, 90, 5, -6, -70, 20, 10, 10, 20, -5, 10, 50, 20, -40]
+        const contentInset = { top: 20, bottom: 20 }
+    */
     return (
         <View style={styles.container}>
-            <View style={{ height: 200, flexDirection: 'row' }}>
-                <YAxis
-                    data={data}
-                    contentInset={contentInset}
-                    svg={{
-                        fill: 'grey',
-                        fontSize: 10,
-                    }}
-                    numberOfTicks={10}
-                    formatLabel={(value) => `${value}ºC`}
-                />
-                <LineChart
-                    style={{ flex: 1, marginLeft: 16 }}
-                    data={data}
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
-                    contentInset={contentInset}
-                >
-                <Grid />
-                </LineChart>
-                <LineChart
-                    style={{ flex: 1, marginLeft: 16 }}
-                    data={data2}
-                    svg={{ stroke: 'rgb(134, 65, 244)' }}
-                    contentInset={contentInset}
-                >
-                <Grid />
-                </LineChart>
-            </View>
-            <View style={styles.list}>
-                <View>          
-                        <Text>Data: 14/05/2020 </Text>
-                        <Text>Pressão: 12/8</Text>
-                </View>
-                <View>
-                    <TouchableOpacity><Text>Editar</Text></TouchableOpacity>
-                    <TouchableOpacity><Text>Excluir</Text></TouchableOpacity>
-                </View>
-            </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
-            </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
-            </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
-            </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
-            </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
-            </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
+            <View style={styles.headerBar}>
+                    <Text style={styles.headerBarText}>Monitor de Pressão</Text>
+                    <TouchableOpacity style={styles.headerBarPerfil}><Text>Perfil</Text></TouchableOpacity>
             </View>
 
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
+            <View style={styles.chartContainer}>
+                <PureChart type={'line'}
+                data={data}
+                width={'100%'}
+                height={200}
+                customValueRenderer={(index, point) => {
+                if (index % 2 === 0) return null
+                return (
+                        <Text style={{textAlign: 'center'}}>{point.y}</Text>
+                )
+                }}/>        
+
             </View>
-            <View>          
-                    <Text>Data: 14/05/2020 </Text>
-                    <Text>Pressão: 12/8</Text>
-            </View>
+            <View style={styles.list}>
+                <View style={styles.listItem}>          
+                          <Text style={styles.listItemTextNormal}>Data: 14/05/2020 </Text>
+                          <Text style={styles.listItemTextDestak}>Pressão: <Text style={styles.preassureTextRed}>12</Text>/<Text style={styles.preassureTextBlue}>8</Text></Text>                        
+                        
+                        <View style={styles.listBoxButton}>
+                                <TouchableOpacity style={styles.editButton}><Text style={styles.listBoxButtonText}>Editar</Text></TouchableOpacity>
+                                <TouchableOpacity style={styles.excludeButton}><Text style={styles.listBoxButtonText}>Excluir</Text></TouchableOpacity>
+                        </View>
+                </View>
+
+                <View style={styles.listItem}>          
+                          <Text style={styles.listItemTextNormal}>Data: 14/05/2020 </Text>
+                          <Text style={styles.listItemTextDestak}>Pressão: <Text style={styles.preassureTextRed}>12</Text>/<Text style={styles.preassureTextBlue}>8</Text></Text>                        
+                        
+                        <View style={styles.listBoxButton}>
+                                <TouchableOpacity style={styles.editButton}><Text style={styles.listBoxButtonText}>Editar</Text></TouchableOpacity>
+                                <TouchableOpacity style={styles.excludeButton}><Text style={styles.listBoxButtonText}>Excluir</Text></TouchableOpacity>
+                        </View>
+                </View>
+
+                <View style={styles.listItem}>          
+                          <Text style={styles.listItemTextNormal}>Data: 14/05/2020 </Text>
+                          <Text style={styles.listItemTextDestak}>Pressão: <Text style={styles.preassureTextRed}>12</Text>/<Text style={styles.preassureTextBlue}>8</Text></Text>                        
+                        
+                        <View style={styles.listBoxButton}>
+                                <TouchableOpacity style={styles.editButton}><Text style={styles.listBoxButtonText}>Editar</Text></TouchableOpacity>
+                                <TouchableOpacity style={styles.excludeButton}><Text style={styles.listBoxButtonText}>Excluir</Text></TouchableOpacity>
+                        </View>
+                </View>
+
+                <View style={styles.listItem}>          
+                          <Text style={styles.listItemTextNormal}>Data: 14/05/2020 </Text>
+                          <Text style={styles.listItemTextDestak}>Pressão: <Text style={styles.preassureTextRed}>12</Text>/<Text style={styles.preassureTextBlue}>8</Text></Text>                        
+                        
+                        <View style={styles.listBoxButton}>
+                                <TouchableOpacity style={styles.editButton}><Text style={styles.listBoxButtonText}>Editar</Text></TouchableOpacity>
+                                <TouchableOpacity style={styles.excludeButton}><Text style={styles.listBoxButtonText}>Excluir</Text></TouchableOpacity>
+                        </View>
+                </View>
+
+             </View>
         </View>  
     )
 }
--- pm_mobile/src/pages/History/styles.js
+++ pm_mobile/src/pages/History/styles.js
@@ -6,11 +6,108 @@ export default StyleSheet.create({
         flex:1,
         paddingHorizontal: 24,
         paddingTop: Constants.statusBarHeight +20,
-        backgroundColor:'#e4e4e4',
+        backgroundColor:'#d4d4d4',
+    },
+    
+    headerBar:{
+        padding: 30,
+        marginBottom: 20,
+
+        flexDirection: "row",
+        justifyContent: "space-between",
+        
+        backgroundColor: "#fff",
+
+        borderRadius: 4,
+    },
+
+    headerBarText:{
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+
+    headerBarPerfil:{
+        maxHeight: 30,
+        maxWidth: 30,
+    },
+
+    chartContainer:{
+        paddingHorizontal: 5,
+        paddingTop: 15,
+        borderRadius: 8,
+        backgroundColor: "#ffff",
     },
 
     list:{
         flex: 1,
-        flexDirection: 'row',
+        marginTop: 20,
+        backgroundColor: "#efefef",
+        borderRadius: 4,
+        paddingHorizontal: 15,
     },
+
+    listItem:{
+       
+        marginTop: 10,  
+        width: "100%",
+        marginRight:30,
+        paddingVertical: 20,
+        backgroundColor: "#fff",
+        borderRadius: 4,
+        paddingHorizontal: 30,
+        shadowColor: "#333333",
+        
+    },
+
+    listItemTextNormal:{
+        fontSize: 18,
+        alignSelf: "center",
+        justifyContent: "space-between",
+    },
+
+    listItemTextDestak:{
+        fontSize: 18,
+        fontWeight: "bold",
+        alignSelf: "center",
+    },
+
+    preassureTextRed:{
+        fontSize:18,
+        fontWeight: "bold",
+        color: "red",
+        marginRight: 5,
+    },
+    preassureTextBlue:{
+        fontSize:18,
+        fontWeight: "bold",
+        color:"blue",
+        marginLeft: 5,
+
+    },
+
+    listBoxButton:{
+        marginTop: 10,
+        flexDirection: 'row',
+        alignItems: "center",   
+        justifyContent: "space-between",
+   },
+
+   listBoxButtonText:{
+        fontSize: 18,
+        fontWeight: "bold",
+   },
+
+   editButton:{
+       backgroundColor: "#4fa7ff",
+       borderRadius: 4,
+       paddingHorizontal: 45,
+       paddingVertical:10,
+   },
+
+   excludeButton:{
+       backgroundColor: "#ff6464",
+       borderRadius: 4,
+       paddingHorizontal: 45,
+       paddingVertical:10,
+   },
 })
